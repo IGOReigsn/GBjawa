2cd94c2bd13c5352ff9617ecb5e727cfc9e25dd2
Igor <eigsn@yandex.ru>
2023-06-30 02:21:51 +0300

    HomeWork4_ready

--- src/main/java/org/example/HomeWork4.java
+++ src/main/java/org/example/HomeWork4.java
@@ -5,9 +5,64 @@
 //        *реализовать сортировку по возрасту с использованием индексов
 //        *реализовать сортировку по возрасту и полу с использованием индексов
 package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class HomeWork4 {
     public static void main(String[] args) {
-
+        ArrayList<Integer> id= new ArrayList<>();
+        ArrayList<String> name =new ArrayList<>();
+        ArrayList<Integer> age =new ArrayList<>();
+        ArrayList<String> gender =new ArrayList<>();
+        id.add(0);
+        name.add("Иванов");
+        age.add(55);
+        gender.add("m");
+        id.add(1);
+        name.add("Иванова");
+        age.add(60);
+        gender.add("f");
+        id.add(2);
+        name.add("Петров");
+        age.add(50);
+        gender.add("m");
+        id.add(3);
+        name.add("Сидоров");
+        age.add(35);
+        gender.add("m");
+        id.add(4);
+        name.add("Кузнецова");
+        age.add(30);
+        gender.add("f");
+        id.add(5);
+        name.add("Смирнов");
+        age.add(25);
+        gender.add("m");
+        id.add(6);
+        name.add("Кузьмина");
+        age.add(20);
+        gender.add("f");
+        System.out.println("Первоначальный список:");
+        for (int i:id) {System.out.println(name.get(i)+" "+age.get(i)+" "+gender.get(i));}
+        Collections.sort(id, new Comparator<Integer>(){
+            public int compare( Integer o1, Integer o2){
+              return age.get(o1)-age.get(o2);
+            }
+        });
+        System.out.println("Отсортированный список:");
+        for (int i:id) {System.out.println(name.get(i)+" "+age.get(i)+" "+gender.get(i));}
+ /*       Collections.sort(id, new Comparator<Integer>(){
+            public int compare( Integer o1, Integer o2){
+                return (age.get(o2)+(if(gender.get(o2)=="f"){200;});)-age.get(o1)+100;
+            }
+        });
+        for (int i:id) {System.out.println(name.get(i)+" "+age.get(i)+" "+gender.get(i));}
+*/
+        Collections.sort(id,(o1,o2)->age.get(o2)-age.get(o1));
+        System.out.println("Обратная сортировка:");
+        for (int i:id) {System.out.println(name.get(i)+" "+age.get(i)+" "+gender.get(i));}
     }
 
 
