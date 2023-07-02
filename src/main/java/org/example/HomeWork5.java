1c78eed0224c9efadd82a4b897c8e4d6473945b5
Igor <eigsn@yandex.ru>
2023-07-02 20:34:55 +0300

    HomeWork5_ready

--- src/main/java/org/example/HomeWork5.java
+++ src/main/java/org/example/HomeWork5.java
@@ -9,8 +9,38 @@
 //        Вывод должен быть отсортирован по убыванию числа телефонов.
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+
 public class HomeWork5 {
     public static void main(String[] args) {
-
-    }
+        HashMap<String, ArrayList<String>> map=new HashMap<>();
+        addphone(map,"Иванов","111-111");
+        addphone(map,"Сидоров","222-111");
+        addphone(map,"Петров","333-111");
+        addphone(map,"Иванов","111-222");
+        addphone(map,"Иванов","111-333");
+        addphone(map,"Петров","333-222");
+        System.out.println("Первоначальный список: "+map);
+        ArrayList<String> list=new ArrayList<>();
+        for(String s: map.keySet()){
+            list.add(s);
+        }
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return map.get(o2).size()-map.get(o1).size();
+            }
+        });
+        System.out.println("Отсортированный список: "+list);
+        System.out.println();
+        System.out.println("Подробный отсортированный список :");
+        list.forEach(n-> System.out.println(n+" : "+ "телефонов = "+map.get(n).size()+"  "+ map.get(n)+" )"));
+    };
+    private static void addphone(HashMap<String,ArrayList<String>> map,String name,String phone){
+            map.putIfAbsent(name, new ArrayList<>());
+            map.get(name).add(phone);
+    };
 }
